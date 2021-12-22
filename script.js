83f44521150a628a652aaac894659cb559496cf6
MiTo0o <76019114+MiTo0o@users.noreply.github.com>
2021-12-22 04:52:31 -0800

    svg name instead of normal font

---
+++ script.js
@@ -0,0 +1,34 @@
+$(function() {
+
+  function animatePath(pathname, animation) {
+    var path = document.querySelector(pathname);
+    var length = path.getTotalLength();
+    // Clear any previous transition
+    path.style.transition = path.style.WebkitTransition =
+      'none';
+    // Set up the starting positions
+    path.style.strokeDasharray = length + ' ' + length;
+    path.style.strokeDashoffset = length;
+    // Trigger a layout so styles are calculated & the browser
+    // picks up the starting position before animating
+    path.getBoundingClientRect();
+    // Define our transition
+    path.style.transition = path.style.WebkitTransition =
+      animation;
+    // Go!
+    path.style.strokeDashoffset = '0';
+  }
+  
+  
+  $("#signature").on({
+    mouseenter: function () {
+
+      animatePath('#sig', 'stroke-dashoffset 10s ease-in-out');
+
+    },
+    mouseleave: function () {
+      console.log(112);
+    }
+  });
+  
+});
