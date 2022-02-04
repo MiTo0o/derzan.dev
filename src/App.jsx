c8eae4d92803fc54a4e825877351db8948b283c8
MiTo0o <76019114+MiTo0o@users.noreply.github.com>
2022-02-04 02:02:37 -0800

    react project started

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+
+import './App.css';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
+import ThemeProvider from '@material-ui/core/styles/ThemeProvider';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
