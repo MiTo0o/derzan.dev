41694a35649400822adbfd6f59337e96847b33cb
MiTo0o <76019114+MiTo0o@users.noreply.github.com>
2022-02-04 23:12:30 -0800

    make bottom logo links clickable

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,16 +4,40 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import BottomNavigation from "@mui/material/BottomNavigation";
 import BottomNavigationAction from "@mui/material/BottomNavigationAction";
 import EmailIcon from '@mui/icons-material/Email';
+
+
 function Footer() {
 
+  const emailTo = 'derzanchiang1800@gmail.com'
+  const bottomNavigationUrls = {
+    'LinkedIn': 'https://www.linkedin.com/in/derzan/' ,
+    'GitHub': 'https://github.com/MiTo0o',
+    'Email': `mailto:${emailTo}`
+  }
   return (
     <BottomNavigation
+      showLabels
+      onChange={(event, newValue) => {
+        window.open(bottomNavigationUrls[newValue], '_blank');
+      }}
       sx={{ position: "fixed", bottom: 0, left: 0, right: 0 }}
       elevation={3}
     >
-      <BottomNavigationAction label="Recents" icon={<LinkedInIcon />} />
-      <BottomNavigationAction label="Favorites" icon={<GitHubIcon />} />
-      <BottomNavigationAction label="Archive" icon={<EmailIcon />} />
+      <BottomNavigationAction
+        label="LinkedIn"
+        value="LinkedIn"
+        icon={<LinkedInIcon />}
+      />
+      <BottomNavigationAction 
+        label="GitHub" 
+        value="GitHub"
+        icon={<GitHubIcon />}
+      />
+      <BottomNavigationAction
+        label="Email"
+        value="Email"
+        icon={<EmailIcon />}
+      />
     </BottomNavigation>
   );
 
